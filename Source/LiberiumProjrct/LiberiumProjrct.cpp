a89f337c55c2f372a0aa3fcbcdca68c9efc66364
kimskyeee <zlagksnf@gmail.com>
2026-03-04 23:28:50 +0900

    Inital test

---
+++ Source/LiberiumProjrct/LiberiumProjrct.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LiberiumProjrct.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LiberiumProjrct, "LiberiumProjrct" );
